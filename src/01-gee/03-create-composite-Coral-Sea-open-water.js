c403a8cd4e16c7712b612be664bdf0c8decbd983
eric.lawrey <eric.lawrey@gmail.com>
2022-02-17 07:58:38 +0000

    Updated src/01-gee/03-create-composite-Coral-Sea-open-water.js (added 2 lines, deleted 1 line).

--- src/01-gee/03-create-composite-Coral-Sea-open-water.js
+++ src/01-gee/03-create-composite-Coral-Sea-open-water.js
@@ -1,3 +1,4 @@
+var s2Utils = require('users/ericlawrey/World_AIMS_Marine-satellite-imagery:src/gee/s2Utils.js');
 // ===============================================================
 //
 //                 CORAL SEA - OPEN WATER
