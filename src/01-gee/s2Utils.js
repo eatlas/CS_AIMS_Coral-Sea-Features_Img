b155b1b84d4ec0234f7a7b22da22bbc4801ad058
eric.lawrey <eric.lawrey@gmail.com>
2022-03-18 10:50:52 +0000

    Updated src/01-gee/s2Utils.js (added 1 line, deleted 1 line).

--- src/01-gee/s2Utils.js
+++ src/01-gee/s2Utils.js
@@ -192,7 +192,7 @@ exports.s2_composite_display_and_export = function(imageIds, is_display, is_expo
     // If the style corresponds to a contour then convert and export as a shapefile
     if (colourGrades[i] === 'ReefTop' || colourGrades[i] === 'Depth10m' || 
       colourGrades[i] === 'Depth5m' || colourGrades[i] === 'DryReef' ||
-      colourGrades[i] === 'Breaking') {
+      colourGrades[i] === 'Breaking' || colourGrades[i] === 'Land') {
       makeAndSaveShp(final_composite, displayName, exportName, exportFolder, exportScale[i], tilesGeometry, is_display, is_export);
     } else {
       
