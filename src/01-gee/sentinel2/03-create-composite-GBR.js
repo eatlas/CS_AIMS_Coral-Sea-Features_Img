dab802edc371787004f7219ea7e569fe9897c922
eric.lawrey <eric.lawrey@gmail.com>
2022-11-04 20:09:40 +0000

    Updated src/01-gee/sentinel2/03-create-composite-GBR.js (added 2 lines).

--- src/01-gee/sentinel2/03-create-composite-GBR.js
+++ src/01-gee/sentinel2/03-create-composite-GBR.js
@@ -6,6 +6,8 @@ var s2Utils = require('users/ericlawrey/CS_AIMS_Coral-Sea-Features_Img:src/01-ge
 
 // Primary imagery
 var REF1_OPTIONS = {
+  //colourGrades: ['DeepFalse','TrueColour','Depth5m', 'Depth10m'],
+  //exportScale: [10, 10, 10, 10],
   colourGrades: ['DeepFalse','TrueColour','Depth5m', 'Depth10m'],
   exportScale: [10, 10, 10, 10],
   exportBasename: 'CS_AIMS_Coral-Sea-Features_Img_S2_R1',
