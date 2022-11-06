2a8601acf38e214ec48698e1865881521a6d1af4
eric.lawrey <eric.lawrey@gmail.com>
2022-11-06 11:19:49 +0000

    Updated src/01-gee/sentinel2/s2Utils.js (added 2 lines, deleted 2 lines).

--- src/01-gee/sentinel2/s2Utils.js
+++ src/01-gee/sentinel2/s2Utils.js
@@ -238,8 +238,8 @@ exports.s2_composite_display_and_export = function(imageIds, is_display, is_expo
         displayMax = 0;
       } else if (colourGrades[i] === 'Depth20m') {
         export_composite = final_composite;
-        displayMin = 0.0375;
-        displayMax = 0.0385;
+        displayMin = 0.0385;
+        displayMax = 0.0395;
       } else {
         // Scale and convert the image to an 8 bit image to make the export
         // file size considerably smaller.
