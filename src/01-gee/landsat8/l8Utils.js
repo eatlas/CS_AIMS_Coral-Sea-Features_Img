ee9e324137fa75b0342307c3680b5f4af66a0461
eric.lawrey <eric.lawrey@gmail.com>
2022-11-08 11:00:13 +0000

    Updated src/01-gee/landsat8/l8Utils.js (added 2 lines, deleted 2 lines).

--- src/01-gee/landsat8/l8Utils.js
+++ src/01-gee/landsat8/l8Utils.js
@@ -346,9 +346,9 @@ var utils = {
         // 115078 0.049     Shark bay (having a lower threshold 0.0483 resulted in a too big border
         //                  and significant noise, essentially we are in the noise).
         resultImage = image.select('B3')
-          // Median filter removes noise but retain edges better than gaussian filter.
+          // Median filter removes noise but retains edges better than mean filter.
           // At the final threshold the median filter can result in small anomalies and
-          // so we apply a small 
+          // so we apply a mean filter to smooth the edges better.
           .focal_median({kernel: ee.Kernel.circle({radius: 90, units: 'meters'}), iterations: 1})
           .focal_mean({kernel: ee.Kernel.circle({radius: 60, units: 'meters'}), iterations: 1})
           .gt(0.049);
