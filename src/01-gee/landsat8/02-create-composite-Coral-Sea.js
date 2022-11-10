9040d844a05623d971ea7235ebd309bb1449510c
eric.lawrey <eric.lawrey@gmail.com>
2022-11-10 23:07:53 +0000

    Updated src/01-gee/landsat8/02-create-composite-Coral-Sea.js (added 12 lines, deleted 53 lines).

--- src/01-gee/landsat8/02-create-composite-Coral-Sea.js
+++ src/01-gee/landsat8/02-create-composite-Coral-Sea.js
@@ -9,6 +9,16 @@
 
 var landsat8Utils = require('users/ericlawrey/CS_AIMS_Coral-Sea-Features_Img:src/01-gee/landsat8/l8Utils.js').utils;
 
+// This script generates Landsat 8 composite images for parts of the 
+// Coral Sea. Landsat 8 coverage in GEE in the Coral Sea seems to be
+// quite limited and so there is incomplete overlap between the Landsat 8
+// imagery and the Sentinel 2 images. 
+// The primary use of this imagery was to map reefs with poor Sentinel 2
+// imagery, in particular Kenn Reefs.
+// It was also used as an independent set of satellite derived bathymetry 
+// that was used to tune the bathymetry calibration (after comparing the
+// initial difference between Landsat 8 and Sentinel 2) and for better
+// assessing the likely level of error in the resulting depth contours.
 
 // These are the options for the primary reference imagery.
 // The primary reference imagery should correspond to a composite
@@ -17,10 +27,8 @@ var landsat8Utils = require('users/ericlawrey/CS_AIMS_Coral-Sea-Features_Img:src
 
 // possible colour grades: ['TrueColour','DeepMarine','DeepFalse','ReefTop', 'Slope', 'Depth5m', 'Depth10m', 'Depth20m' ]
 var REF1_OPTIONS = {
-    // colourGrades: ['TrueColour', 'DeepFalse', 'Depth5m', 'Depth10m', 'Depth20m'],
-    // exportScale: [30, 30, 30, 30, 30],
-    colourGrades: ['Depth5m', 'Depth10m', 'Depth20m'],
-    exportScale: [30, 30, 30],
+    colourGrades: ['TrueColour', 'DeepFalse', 'Depth5m', 'Depth10m', 'Depth20m'],
+    exportScale: [30, 30, 30, 30, 30],
     exportBasename: 'CS_AIMS_Coral-Sea-Features_Img_L8_R1',
     exportFolder: 'EarthEngine/CS_AIMS_Coral-Sea-Features_Img/Coral-Sea',
     applySunGlintCorrection: true,
@@ -239,54 +247,5 @@ landsat8Utils.composeDisplayAndExport(
         "LANDSAT/LC08/C02/T1_TOA/LC08_087074_20200822",
         "LANDSAT/LC08/C02/T1_TOA/LC08_087074_20151113",
         "LANDSAT/LC08/C02/T1_TOA/LC08_087074_20170814"
-    ],
-    false, true, REF1_OPTIONS);
-
-
-// ===============================================================
-// Sharkbay
-// This area was used for tuning the B2_OFFSET parameter in the Depth
-// estimation. This area was used due to the large seagrass meadows
-// in clear water on sand. The B2_OFFSET was tuned so that seagrass areas
-// would result in a similar depth to the immediately neighbouring sandy areas,
-// working off the assumption that they are probably a similar depth.
-landsat8Utils.composeDisplayAndExport(
-    [
-        "LANDSAT/LC08/C02/T1_TOA/LC08_115078_20150627",
-        "LANDSAT/LC08/C02/T1_TOA/LC08_115078_20130621",
-        "LANDSAT/LC08/C02/T1_TOA/LC08_115078_20150814",
-        "LANDSAT/LC08/C02/T1_TOA/LC08_115078_20130824"
-    ],
-    false, false, REF1_OPTIONS);
-    
-// Depth calibration images.
-// These scenes were used to calibrate the depth calculations against the GA GBR30 2020
-// dataset. No tidal assessment was made. For this calibration the Depth styles were used.
-// 
-
-// Davies reef
-landsat8Utils.composeDisplayAndExport(
-    [
-        "LANDSAT/LC08/C02/T1_TOA/LC08_094073_20190822",
-        "LANDSAT/LC08/C02/T1_TOA/LC08_094073_20190907"
-    ],
-    false, true, REF1_OPTIONS);
-    
-// Tongue and Batt Reef
-landsat8Utils.composeDisplayAndExport(
-    [
-        "LANDSAT/LC08/C02/T1_TOA/LC08_096071_20160608",
-        "LANDSAT/LC08/C02/T1_TOA/LC08_096071_20180801",
-        "LANDSAT/LC08/C02/T1_TOA/LC08_096071_20190905",
-        "LANDSAT/LC08/C02/T1_TOA/LC08_096071_20130702"
-    ],
-    false, true, REF1_OPTIONS);
-    
-    
-// Paul Reef
-landsat8Utils.composeDisplayAndExport(
-    [
-        "LANDSAT/LC08/C02/T1_TOA/LC08_091075_20140903",
-        "LANDSAT/LC08/C02/T1_TOA/LC08_091075_20180829"
     ],
     false, true, REF1_OPTIONS);
